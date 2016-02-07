a00c816d050fedaeefb4a330a996c543a72f66ff
Chris Wood <chrisdotwood+github@gmail.com>
2016-02-07 21:12:01 +0000

    Added test of cascading delete of reading log

--- FlipBookLog.Tests/DatabaseContextTests.cs
+++ FlipBookLog.Tests/DatabaseContextTests.cs
@@ -24,5 +24,26 @@ namespace Flip.Tests {
 			Assert.True(_databaseFixture.Context.Books.Count() > 0, "No books in database");
 		}
 
+		[Fact]
+		public void DeleteOfReadingLogCascades() {
+			int readingLogCount = _databaseFixture.Context.ReadingLogs.Count();
+			int bookCount = _databaseFixture.Context.Books.Count();
+
+            Assert.True(readingLogCount > 0, "No reading logs in database");
+			Assert.True(bookCount > 0, "No books in database");
+
+			// Check that the first log actually has some books associated with it
+			Assert.True(_databaseFixture.Context.ReadingLogs.First().Books.Count() > 0);
+
+			// Remove the reading log. This should remove the record of the books that are associated with it too.
+			_databaseFixture.Context.ReadingLogs.Remove(_databaseFixture.Context.ReadingLogs.First());
+
+			// Persist the changes to the database
+			_databaseFixture.Context.SaveChanges();
+
+			// Check that the log and associated books are now gone
+			Assert.Equal(readingLogCount - 1, _databaseFixture.Context.ReadingLogs.Count());
+			Assert.True(_databaseFixture.Context.Books.Count() < bookCount);
+		}
 	}
 }
