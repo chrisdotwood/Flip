a5f0b8063385a0ef26e01dfdcdd45fbc38ad511d
Chris Wood <chrisdotwood+github@gmail.com>
2016-09-18 12:11:18 +0100

    - Removed redundant MVC stuff

--- FlipBookLog/Controllers/AccountController.cs
+++ FlipBookLog/Controllers/AccountController.cs
@@ -1,70 +1,14 @@
-﻿using Flip.DomainModel;
-using Flip.Repository;
-using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
-using System.Web.Http;
+using System.Web.Mvc;
 
 namespace Flip.Controllers {
-	[RoutePrefix("api/Account")]
-	public class AccountController : ApiController {
-		private AuthRepository _repo = null;
-
-		public AccountController() {
-			_repo = new AuthRepository();
-		}
-
-		// POST api/Account/Register
-		[AllowAnonymous]
-		[Route("Register")]
-		public async Task<IHttpActionResult> Register(FlipUser userModel) {
-			if (!ModelState.IsValid) {
-				return BadRequest(ModelState);
-			}
-
-			IdentityResult result = await _repo.RegisterUser(userModel);
-
-			IHttpActionResult errorResult = GetErrorResult(result);
-
-			if (errorResult != null) {
-				return errorResult;
-			}
-
-			return Ok();
-		}
-
-		protected override void Dispose(bool disposing) {
-			if (disposing) {
-				_repo.Dispose();
-			}
-
-			base.Dispose(disposing);
-		}
-
-		private IHttpActionResult GetErrorResult(IdentityResult result) {
-			if (result == null) {
-				return InternalServerError();
-			}
-
-			if (!result.Succeeded) {
-				if (result.Errors != null) {
-					foreach (string error in result.Errors) {
-						ModelState.AddModelError("", error);
-					}
-				}
-
-				if (ModelState.IsValid) {
-					// No ModelState errors are available to send, so just return an empty BadRequest.
-					return BadRequest();
-				}
-
-				return BadRequest(ModelState);
-			}
-
-			return null;
+	public class AccountController : Controller {
+		// GET: Account
+		public ActionResult Index() {
+			return View();
 		}
 	}
 }
